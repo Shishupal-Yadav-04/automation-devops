0514c76252cca02b017ae99c4563e9c56e3096f2
Shishupal-Yadav <shishupal614@gmail.com>
2025-06-14 17:53:23 +0530

    vpc-subnet-igw-rt

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
+module "vpc" {
+  source          = "./vpc"
+  region          = var.aws_region
+  vpc_cidr        = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  tags = {
+    Project     = "awsproj"
+    Environment = "dev"
+  }
+}
+
 module "iam_role" {
   source         = "./iam"
   role_name      = var.role_name
   aws_account_id = var.aws_account_id
+  tags = {
+    Project     = "awsproj"
+    Environment = "dev"
+  }
 }
+
---
+++ vpc/main.tf
@@ -0,0 +1,67 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = merge(var.tags, {
+    Name = "awsproj-vpc"
+  })
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {
+    Name = "awsproj-igw"
+  })
+}
+
+
+/*
+The count meta-argument is used to dynamically create multiple resources based on a list.
+length(var.public_subnets) tells Terraform to create as many subnets as there are CIDRs in the list.
+*/
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.region}a"
+  tags = merge(var.tags, {
+    Name = "awsproj-public-${count.index + 1}"
+  })
+}
+
+/*
+Purpose of merge():
+It combines two or more maps into one.
+var.tags comes from the root module (typically includes common tags like Project and Environment).I
+*/
+
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = "${var.region}b"
+  tags = merge(var.tags, {
+    Name = "awsproj-private-${count.index + 1}"
+  })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {
+    Name = "awsproj-public-rt"
+  })
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
